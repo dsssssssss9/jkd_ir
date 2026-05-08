81c3b8abaaa66d48a953dc1feb04be3e29832166
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2026-05-08 18:05:32 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+makerbit.onIrButton(IrButton.Left, IrButtonAction.Pressed, function () {
+    basic.showArrow(ArrowNames.West)
+})
+makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
+    basic.showArrow(ArrowNames.South)
+})
+makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
+    basic.showArrow(ArrowNames.North)
+})
+makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
+    basic.showArrow(ArrowNames.East)
+})
+makerbit.onIrButton(IrButton.Ok, IrButtonAction.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+})
+makerbit.connectIrReceiver(DigitalPin.P1, IrProtocol.NEC)
